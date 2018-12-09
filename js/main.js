56279c79b626615ecb2f23be92f2e26f1486aee0
joshiadvait8 <joshiadvait8@gmail.com>
2018-12-09 16:46:32 +0530

    events updateed from firebase

--- js/main.js
+++ js/main.js
@@ -143,5 +143,23 @@ jQuery(document).ready(function( $ ) {
   })
 
 // custom code
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+}) 
 
+
+ 
 });
